2b1a00a8337621c5eec02b676e0ca2cf66fb62d9
NikKowPHP <nik.kow@outlook.com>
2025-06-13 09:43:22 +0200

    Refactor: Remove legacy project files and configurations
    This commit removes a large number of files and configurations related to the previous project structure. This includes removing NestJS application files, Docker configurations, documentation, Prisma schema, and other related files. The removal of these files is necessary to clean up the project and prepare it for the new architecture. New files related to the new architecture are added under `.roo/` directory.

--- src/post-triggers/dto/update-post-trigger.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostTriggerDto } from './create-post-trigger.dto';
-
-export class UpdatePostTriggerDto extends PartialType(CreatePostTriggerDto) {}
